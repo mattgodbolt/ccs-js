a907e5c67418a3de60f13f89cacc143d6e164d0a
Matt Godbolt <mgodbolt@drwholdings.com>
2014-05-05 22:43:58 -0500

    More work in progress on the parser. Slowly taking shape

--- test/parser_test.js
+++ test/parser_test.js
@@ -1,116 +1,129 @@
 var parserLib = require('../lib/ccs').parser;
 
 var assert = require("assert");
+var parser = new parserLib.Parser();
 describe('Parser', function(){
+    function P(a) { parser.parse(a); }
+    function F(a) { assert.throws(function(){parser.parse(a);}); }
+
     describe('BasicPhrases', function(){
         var parser = new parserLib.Parser();
         it('should parse empty strings', function(){
-            assert.equal(true, parser.parse(""));
-            assert.equal(true, parser.parse("   "));
-            assert.equal(true, parser.parse("\n\t "));
+            P("");
+            P("   ");
+            P("\n\t ");
         });
         it('should parse // comments', function(){
-            assert.equal(true, parser.parse("// this is a comment"));
-            assert.equal(true, parser.parse("// this\n//is\n//many\n//comments"));
-            assert.equal(true, parser.parse("  // comment and whitespace"));
+            P("// this is a comment");
+            P("// this\n//is\n//many\n//comments");
+            P("  // comment and whitespace");
         });
         it('should parse /* comments */', function(){
-            assert.equal(true, parser.parse("/* this is a comment */"));
-            assert.equal(true, parser.parse("/* this\nis\na\ncomment*/"));
-            assert.equal(true, parser.parse("  /* comment and whitespace */  "));
+            P("/* this is a comment */");
+            P("/* this\nis\na\ncomment*/");
+            P("  /* comment and whitespace */  ");
         });
         it('should parse nested /* comments */', function(){
-            assert.equal(true, parser.parse("/* /* this isn't nice */ but is legal */"));
+            P("/* /* this isn't nice */ but is legal */");
         });
         it('should accept import files', function(){
-            assert.equal(true, parser.parse("@import 'file'"));
+            P("@import 'file'");
         });
         it('should accept context lines', function(){
-            assert.equal(true, parser.parse("@context (foo.bar > baz)"));
-            assert.equal(true, parser.parse("@context (foo x.bar > baz >)"));
+            P("@context (foo.bar > baz)");
+            P("@context (foo x.bar > baz >)");
         });
         it('should accept property defs', function(){
-            assert.equal(true, parser.parse("prop = 'val'"));
+            P("prop = 'val'");
         });
         it('should accept simple elem defs', function(){
-            assert.equal(true, parser.parse("elem.id {}"));
-            assert.equal(true, parser.parse("elem.id {prop = 'val'}"));
+            P("elem.id {}");
+            P("elem.id {prop = 'val'}");
         });
         it('should parse properties in nested defs - int', function(){
-            assert.equal(true, parser.parse("a.class.class blah > elem.id { prop = 43 }"));
+            P("a.class.class blah > elem.id { prop = 43 }");
         });
         it('should parse properties in nested defs - float', function(){
-            assert.equal(true, parser.parse("a.class.class blah > elem.id { prop = 2.3 }"));
+            P("a.class.class blah > elem.id { prop = 2.3 }");
         });
         it('should parse properties in nested defs - string', function(){
-            assert.equal(true, parser.parse("a.class.class blah > elem.id { prop = \"val\" }"));
+            P("a.class.class blah > elem.id { prop = \"val\" }");
         });
         it('should parse properties in nested defs - hex', function(){
-            assert.equal(true, parser.parse("a.class.class blah > elem.id { prop = 0xAB12 }"));
+            P("a.class.class blah > elem.id { prop = 0xAB12 }");
         });
         it('should parse properties in nested defs - bool', function(){
-            assert.equal(true, parser.parse("a.class.class blah > elem.id { prop = true }"));
-            assert.equal(true, parser.parse("a.class.class blah > elem.id { prop = false }"));
+            P("a.class.class blah > elem.id { prop = true }");
+            P("a.class.class blah > elem.id { prop = false }");
         });
-        it('should fail to parse duplicate class suffixes', function(){
-            assert.equal(false, parser.parse("a.class.class blah > elem. id {prop=2.3}"));
-            assert.equal(false, parser.parse("a.class.class blah > elem.id { prop = false }"));
+        it('should fail to parse properties with spacing', function(){
+            F("a.class.class blah > elem. id {prop=2.3}");
+            F("a.class. class blah > elem.id { prop = false }");
+        });
+        it('should not parse just an identifier', function(){
+            F("blah");
+        });
+        it('should not parse contexts away from the top', function(){
+            F("@import 'file'; @context (foo)");
+            F("a.class { @context (foo) }");
+        });
+        it('should allow constraints later on', function(){
+            P("@import 'file' ; @constrain foo");
+        });
+        it('should allow imports within contexts', function(){
+            P("@import 'file'; @import 'foo'");
+        });
+        it('should parse two properties', function(){
+            P("elem.id { prop = 'val'; prop2 = 31337 }");
         });
     })
 })
-//  EXPECT_FALSE(parser.parse("a.class.class blah > elem. id {prop=2.3}"));
-//  EXPECT_FALSE(parser.parse("a.class. class > elem.id {prop=\"val\"}"));
-//  EXPECT_FALSE(parser.parse("blah"));
-//  EXPECT_FALSE(parser.parse("@import 'file'; @context (foo)"));
-//  EXPECT_TRUE(parser.parse("@import 'file' ; @constrain foo"));
-//  EXPECT_TRUE(parser.parse("a.class { @import 'file' }"));
-//  EXPECT_FALSE(parser.parse("a.class { @context (foo) }"));
-//  EXPECT_TRUE(parser.parse("elem.id { prop = 'val'; prop2 = 31337 }"));
-//  EXPECT_TRUE(parser.parse("prop.'val'/a.foo/p.'hmm' { p = 1; }"));
-//  EXPECT_TRUE(parser.parse("a b > c d {p=1}"));
-//  EXPECT_TRUE(parser.parse("(a > b) (c > d) {p=1}"));
-//  EXPECT_TRUE(parser.parse("a > (b c) > d {p=1}"));
-//  EXPECT_TRUE(parser.parse("a.\"foo\" 'bar' {'test' = 1};"));
+//  EXPECT_TRUE(parser.parse("elem.id { prop = 'val'; prop2 = 31337 }");
+//  EXPECT_TRUE(parser.parse("prop.'val'/a.foo/p.'hmm' { p = 1; }");
+//  EXPECT_TRUE(parser.parse("a b > c d {p=1}");
+//  EXPECT_TRUE(parser.parse("(a > b) (c > d) {p=1}");
+//  EXPECT_TRUE(parser.parse("a > (b c) > d {p=1}");
+//  EXPECT_TRUE(parser.parse("a.\"foo\" 'bar' {'test' = 1};");
 //}
 //
 //TEST(ParserTest, Comments) {
 //  P parser;
-//  EXPECT_TRUE(parser.parse("// single line comment\n"));
-//  EXPECT_TRUE(parser.parse("// single line comment nonl"));
-//  EXPECT_TRUE(parser.parse("/* multi-line comment */"));
-//  EXPECT_TRUE(parser.parse("prop = /* comment */ 'val'"));
-//  EXPECT_TRUE(parser.parse("prop = /* comment /*nest*/ more */ 'val'"));
-//  EXPECT_TRUE(parser.parse("elem.id /* comment */ {prop = 'val'}"));
-//  EXPECT_TRUE(parser.parse("// comment\nelem { prop = 'val' prop = 'val' }"));
+//  EXPECT_TRUE(parser.parse("// single line comment\n");
+//  EXPECT_TRUE(parser.parse("// single line comment nonl");
+//  EXPECT_TRUE(parser.parse("/* multi-line comment */");
+//  EXPECT_TRUE(parser.parse("prop = /* comment */ 'val'");
+//  EXPECT_TRUE(parser.parse("prop = /* comment /*nest*/ more */ 'val'");
+//  EXPECT_TRUE(parser.parse("elem.id /* comment */ {prop = 'val'}");
+//  EXPECT_TRUE(parser.parse("// comment\nelem { prop = 'val' prop = 'val' }");
 //}
 //
 //TEST(ParserTest, UglyAbutments) {
 //  P parser;
-//  EXPECT_FALSE(parser.parse("foo {p = 1x = 2}"));
-//  EXPECT_FALSE(parser.parse("foo {p = 'x'x = 2}"));
-//  EXPECT_FALSE(parser.parse("value=12env.foo {}"));
-//  EXPECT_TRUE(parser.parse("foo {p = 1 x = 2}"));
-//  EXPECT_TRUE(parser.parse("foo{p=1;x=2}"));
-//  EXPECT_FALSE(parser.parse("foo{@overridep=1}"));
-//  EXPECT_TRUE(parser.parse("foo{@override /*hi*/ p=1}"));
-//  EXPECT_FALSE(parser.parse("@import'asdf'"));
-//  EXPECT_FALSE(parser.parse("@constrainasdf"));
+//  EXPECT_FALSE(parser.parse("foo {p = 1x = 2}");
+//  EXPECT_FALSE(parser.parse("foo {p = 'x'x = 2}");
+//  EXPECT_FALSE(parser.parse("value=12env.foo {}");
+//  EXPECT_TRUE(parser.parse("foo {p = 1 x = 2}");
+//  EXPECT_TRUE(parser.parse("foo{p=1;x=2}");
+//  EXPECT_FALSE(parser.parse("foo{@overridep=1}");
+//  EXPECT_TRUE(parser.parse("foo{@override /*hi*/ p=1}");
+//  EXPECT_FALSE(parser.parse("@import'asdf'");
+//  EXPECT_FALSE(parser.parse("@constrainasdf");
 //  EXPECT_TRUE(parser.parse(
-//      "@import 'asdf' \n ; \n @constrain asdf \n ; @import 'foo'  "));
-//  EXPECT_TRUE(parser.parse("@import /*hi*/ 'asdf'"));
+//      "@import 'asdf' \n ; \n @constrain asdf \n ; @import 'foo'  ");
+//  EXPECT_TRUE(parser.parse("@import /*hi*/ 'asdf'");
 //}
 //
 //TEST(ParserTest, SelectorSections) {
 //  P parser;
-//  EXPECT_TRUE(parser.parse("foo > { bar {}}"));
-//  EXPECT_TRUE(parser.parse("foo > { bar > baz {}}"));
-//  EXPECT_TRUE(parser.parse("bar > baz {}"));
-//  EXPECT_TRUE(parser.parse("bar baz {}"));
+//  EXPECT_TRUE(parser.parse("foo > { bar {}}");
+//  EXPECT_TRUE(parser.parse("foo > { bar > baz {}}");
+//  EXPECT_TRUE(parser.parse("bar > baz {}");
+//  EXPECT_TRUE(parser.parse("bar baz {}");
 //}
 //
 //TEST(ParserTest, Constraints) {
 //  P parser;
-//  EXPECT_TRUE(parser.parse("a.b: @constrain a.c"));
+//  EXPECT_TRUE(parser.parse("a.b: @constrain a.c");
 //}
 //
 //
